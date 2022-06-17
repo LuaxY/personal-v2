98a5a04d2f0a8a573adeb88f8041300aaf314c4a
Yann Guineau <yann@chainsafe.io>
2022-06-16 22:46:05 -0400

    Add illustration in Me section

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="md:pt-26 mx-auto grid max-w-5xl grid-cols-1 py-10 md:grid-cols-2 md:py-20">
-    <div class="px-5 md:px-0">
+  <div class="mx-auto grid max-w-6xl grid-cols-1 items-center pt-10 pb-5 md:grid-cols-2 md:pb-20 md:pt-5">
+    <div class="justify-self-end px-5 md:px-0">
       <div class="flex flex-col gap-y-3">
         <div class="text-xl md:text-3xl">Hello!</div>
         <h1 class="text-4xl md:text-6xl">I'm <span class="font-medium text-emerald-500 dark:text-yellow-500">Yann Guineau</span></h1>
@@ -18,7 +18,15 @@
         </div>
       </div>
     </div>
-    <div></div>
+    <div class="flex justify-end">
+      <svg viewBox="0 0 500 500" width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+        <path d="M 219.313 -1.479 Q 238.198 -12.382 257.083 -1.479 L 408.167 85.749 Q 427.052 96.653 427.052 118.46 L 427.052 292.916 Q 427.052 314.723 408.167 325.626 L 257.083 412.855 Q 238.198 423.758 219.313 412.855 L 68.229 325.627 Q 49.344 314.723 49.344 292.916 L 49.344 118.46 Q 49.344 96.653 68.229 85.749 Z" stroke-width="4" stroke="currentColor" fill="none" class="text-emerald-500 dark:text-yellow-500" transform="matrix(0, 1, -1, 0, 455, 50)" />
+        <path d="M 229.68 112.244 Q 238.198 107.326 246.716 112.244 L 314.864 151.589 Q 323.382 156.507 323.382 166.343 L 323.382 245.033 Q 323.382 254.869 314.864 259.787 L 246.716 299.132 Q 238.198 304.05 229.68 299.132 L 161.532 259.787 Q 153.014 254.869 153.014 245.033 L 153.014 166.343 Q 153.014 156.507 161.532 151.589 Z" stroke-width="0" stroke="none" fill="currentColor" class="text-emerald-400 dark:text-yellow-400" transform="matrix(0, 1, -1, 0, 340, -10)" />
+        <path d="M 229.68 112.244 Q 238.198 107.326 246.716 112.244 L 314.864 151.589 Q 323.382 156.507 323.382 166.343 L 323.382 245.033 Q 323.382 254.869 314.864 259.787 L 246.716 299.132 Q 238.198 304.05 229.68 299.132 L 161.532 259.787 Q 153.014 254.869 153.014 245.033 L 153.014 166.343 Q 153.014 156.507 161.532 151.589 Z" stroke-width="0" stroke="none" fill="currentColor" class="text-emerald-600 dark:text-yellow-600" transform="matrix(0, 1, -1, 0, 550, -120)" />
+        <path d="M 229.68 112.244 Q 238.198 107.326 246.716 112.244 L 314.864 151.589 Q 323.382 156.507 323.382 166.343 L 323.382 245.033 Q 323.382 254.869 314.864 259.787 L 246.716 299.132 Q 238.198 304.05 229.68 299.132 L 161.532 259.787 Q 153.014 254.869 153.014 245.033 L 153.014 166.343 Q 153.014 156.507 161.532 151.589 Z" stroke-width="0" stroke="none" fill="currentColor" class="text-emerald-300 dark:text-yellow-300" transform="matrix(0, 1, -1, 0, 570, 120)" />
+        <path d="M 233.395 153.002 Q 238.198 150.229 243.001 153.002 L 281.424 175.186 Q 286.227 177.959 286.227 183.504 L 286.227 227.872 Q 286.227 233.418 281.424 236.19 L 243.001 258.374 Q 238.198 261.147 233.395 258.374 L 194.972 236.19 Q 190.169 233.418 190.169 227.872 L 190.169 183.504 Q 190.169 177.959 194.972 175.186 Z" stroke-width="0" stroke="none" fill="currentColor" class="text-emerald-700 dark:text-yellow-700" transform="matrix(0, 1, -1, 0, 385, 170)" />
+      </svg>
+    </div>
   </div>
 </template>
 
